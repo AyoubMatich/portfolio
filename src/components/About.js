3ecff2a8fb3399f03ea3aad803f3e094ec84009d
ayyoub <matichayyoub88@gmail.com>
2024-09-02 23:26:14 +0200

    coding about

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Hi, I'm a web developer with a passion for building responsive and user-friendly websites.</p>
+    </section>
+  );
+}
+
+export default About;
